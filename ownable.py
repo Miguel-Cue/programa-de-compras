37e11f3f324ad6b2b62fb4ab3fbbf57889b7cd22
Miguel-Cue <ayalacuevas53@gmail.com>
2024-07-15 15:18:28 -0400

    Update ownable.py

--- ownable.py
+++ ownable.py
@@ -1,2 +1,14 @@
-def set_owner(self, owner):
-    self.owner = owner
+# ownable.py
+class Ownable:
+    def __init__(self, owner):
+        self.owner = owner
+
+    def is_owner(self, user):
+        return user == self.owner
+
+    def require_owner(self, user):
+        if not self.is_owner(user):
+            raise PermissionError("User does not have the necessary privileges.")
+    
+    def set_owner(self, owner):
+        self.owner = owner
