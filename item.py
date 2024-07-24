18d61312c2bdeae6b5e27c5dc9a58348aaedf4c1
Miguel-Cue <ayalacuevas53@gmail.com>
2024-07-24 09:53:21 -0400

    Update item.py

--- item.py
+++ item.py
@@ -1,14 +1,16 @@
+# item.py
 class Item:
     instances = []
 
-    def __init__(self, name, price, owner=None):
+    def __init__(self, name, price, owner=None, quantity=1):
         self.name = name
         self.price = price
-        self.owner = owner 
+        self.owner = owner
+        self.quantity = quantity
         Item.instances.append(self)
 
     def label(self):
-        return {"name": self.name, "price": self.price}
+        return {"name": self.name, "price": self.price, "quantity": self.quantity}
 
     @staticmethod
     def item_all():
