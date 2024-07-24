3bf6dfd0e1d611b75154e3197126a76b39e2dbfb
Miguel-Cue <ayalacuevas53@gmail.com>
2024-07-24 09:52:27 -0400

    Update shopping_app.py

--- shopping_app.py
+++ shopping_app.py
@@ -4,34 +4,31 @@ from item import Item
 from seller import Seller
 
 def main():
-    # Crear instancia de Seller y agregar artículos
     seller = Seller("DICストア")
-    seller.add_item(Item("CPU", 40830, seller))
-    seller.add_item(Item("Memoria RAM", 13880, seller))
-    seller.add_item(Item("Placa madre", 28980, seller))
-    seller.add_item(Item("Unidad de alimentación", 8980, seller))
-    seller.add_item(Item("Caja de PC", 8727, seller))
-    seller.add_item(Item("HDD de 3.5 pulgadas", 10980, seller))
-    seller.add_item(Item("SSD de 2.5 pulgadas", 13370, seller))
-    seller.add_item(Item("SSD M.2", 12980, seller))
-    seller.add_item(Item("Refrigeración CPU", 13400, seller))
-    seller.add_item(Item("Tarjeta gráfica", 23800, seller))
+    seller.add_item(Item("CPU", 40830, seller, 10))
+    seller.add_item(Item("Memoria RAM", 13880, seller, 20))
+    seller.add_item(Item("Placa madre", 28980, seller, 15))
+    seller.add_item(Item("Unidad de alimentación", 8980, seller, 30))
+    seller.add_item(Item("Caja de PC", 8727, seller, 25))
+    seller.add_item(Item("HDD de 3.5 pulgadas", 10980, seller, 18))
+    seller.add_item(Item("SSD de 2.5 pulgadas", 13370, seller, 22))
+    seller.add_item(Item("SSD M.2", 12980, seller, 16))
+    seller.add_item(Item("Refrigeración CPU", 13400, seller, 12))
+    seller.add_item(Item("Tarjeta gráfica", 23800, seller, 8))
 
-    # Solicitar nombre y cargar billetera del cliente
     print("🤖 Por favor, ingresa tu nombre:")
     customer = Customer(input())
 
     print("🏧 Ingresa la cantidad para cargar en tu billetera:")
     amount_str = input()
-    while not amount_str.isdigit():  # Verifica si la cadena contiene solo dígitos
+    while not amount_str.isdigit():
         print("⚠️ Por favor ingresa solo números. Intenta nuevamente:")
         amount_str = input()
 
-    customer.wallet.deposit(int(amount_str))  # Convierte la cantidad válida a entero y la deposita
+    customer.wallet.deposit(int(amount_str))
 
     print("🛍️ Comenzando compras...")
 
-    # Mostrar la lista de productos disponibles una sola vez al inicio
     def show_available_products():
         print("📜 Lista de productos disponibles:")
         seller.mostrar_productos()
@@ -43,20 +40,20 @@ def main():
         print("️️⛏ Ingresa el número del producto que deseas comprar:")
         number_str = input()
 
-        while not number_str.isdigit():  # Verifica si la cadena contiene solo dígitos
+        while not number_str.isdigit():
             print("⚠️ Por favor, ingresa un número válido. Intenta nuevamente:")
             number_str = input()
 
-        number = int(number_str)  # Convierte la cantidad válida a entero
+        number = int(number_str)
 
         print("⛏ Ingresa la cantidad de productos que deseas comprar:")
         quantity_str = input()
 
-        while not quantity_str.isdigit():  # Verifica si la cadena contiene solo dígitos
+        while not quantity_str.isdigit():
             print("⚠️ Por favor, ingresa un número válido. Intenta nuevamente:")
             quantity_str = input()
 
-        quantity = int(quantity_str)  # Convierte la cantidad válida a entero
+        quantity = int(quantity_str)
 
         items = seller.pick_items(number, quantity)
         for item in items:
@@ -70,7 +67,6 @@ def main():
         end_shopping = input() == "yes"
 
         if not end_shopping:
-            # Mostrar la lista de productos disponibles cada vez que el usuario decida seguir comprando
             show_available_products()
 
     print("💸 ¿Deseas confirmar la compra? (yes/no)")
@@ -82,17 +78,17 @@ def main():
         else:
             print("😱 No hay suficiente saldo en la billetera para completar la compra.")
 
-    # Mostrar resultados finales
     print("୨୧┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ Resultado ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈୨୧")
     print(f"️🛍️ Artículos de {customer.name}:")
-    customer.cart.show_items()  # Mostrar los artículos en el carrito
+    customer.cart.show_items()
     print(f"😱👛 Saldo actual en la billetera de {customer.name}: {customer.wallet.balance}")
 
-    # Mostrar el saldo del vendedor
     print(f"😻👛 Saldo actual en la billetera de {seller.name}: {seller.wallet.balance}")
 
-    # Mostrar el total a pagar al final
     print(f"🌚 Total a pagar: {total_amount}")
+
+    print("📜 Productos restantes en la tienda:")
+    seller.mostrar_productos()
 
     print("🎉 Fin del programa")
 
