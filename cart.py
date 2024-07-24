87262fcc1aea0696b40f8d100f488a59d2b35fb9
Miguel-Cue <ayalacuevas53@gmail.com>
2024-07-24 16:59:48 -0400

    Update cart.py

--- cart.py
+++ cart.py
@@ -18,15 +18,15 @@ class Cart:
     def check_out(self):
         if self.owner.wallet.balance < self.total_amount():
             print("Fondos insuficientes.")
-            return
-
+            return False 
+        
         for item in self.items:
             item.owner.wallet.withdraw(item.price)
             self.owner.wallet.deposit(item.price)
             item.owner = self.owner
         
         self.items = []
-        print("Pago exitoso.")
+        return True 
 
     def show_items(self):
         if not self.items:
@@ -39,7 +39,10 @@ class Cart:
                 else:
                     grouped_items[item.name] = {'price': item.price, 'quantity': 1}
 
+            # Crear los datos para el formato tabular
             table_data = []
             for index, (name, details) in enumerate(grouped_items.items(), start=1):
                 table_data.append([index, name, details['price'], details['quantity']])
+
+            # Mostrar los datos en formato tabular
             print(tabulate(table_data, headers=["N°", "Producto", "Precio", "Cantidad"], tablefmt="grid"))
