4a556fb8db0361d8ec1f9c2659ac5a48d0d2a548
Miguel-Cue <ayalacuevas53@gmail.com>
2024-07-24 09:52:45 -0400

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,3 +1,4 @@
+# cart.py
 from tabulate import tabulate
 
 class Cart:
@@ -18,7 +19,7 @@ class Cart:
         if self.owner.wallet.balance < self.total_amount():
             print("Fondos insuficientes.")
             return
-        
+
         for item in self.items:
             item.owner.wallet.withdraw(item.price)
             self.owner.wallet.deposit(item.price)
@@ -31,7 +32,6 @@ class Cart:
         if not self.items:
             print("El carrito está vacío.")
         else:
-            # Crear un diccionario para agrupar los productos por nombre
             grouped_items = {}
             for item in self.items:
                 if item.name in grouped_items:
@@ -39,10 +39,7 @@ class Cart:
                 else:
                     grouped_items[item.name] = {'price': item.price, 'quantity': 1}
 
-            # Crear los datos para el formato tabular
             table_data = []
             for index, (name, details) in enumerate(grouped_items.items(), start=1):
                 table_data.append([index, name, details['price'], details['quantity']])
-
-            # Mostrar los datos en formato tabular
             print(tabulate(table_data, headers=["N°", "Producto", "Precio", "Cantidad"], tablefmt="grid"))
